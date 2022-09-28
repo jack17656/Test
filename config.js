8e84475671f2f0e018976b8728ca3b7207ed522c
jack17656 <114502766+jack17656@users.noreply.github.com>
2022-09-28 20:49:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,11 @@
 const CONFIG = {
+    titleWeb: "Tiêu đề web",
+    introTitle: 'Tên người yêu bạn',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
     title: 'Would you like to hear our group's presentation?',
     desc: 'Leaving without an answer is consider a yes :))',
     btnYes: 'Absolutely',
